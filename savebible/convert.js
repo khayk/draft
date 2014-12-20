3ec4808664cc8e4e8a8806e559153115e5e93c4f
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2014-12-20 13:29:22 +0400

    .

--- savebible/convert.js
+++ savebible/convert.js
@@ -1,17 +1,62 @@
-var moduleBible = require('./lib-modules/bible.js');
-
-var Verse   = moduleBible.Verse;
-var Chapter = moduleBible.Chapter;
-
-//var verse = new moduleBible.Verse();
-
-var v = new Verse();
-v.render();
-
-var c = new Chapter();
-c.render();
-
 (function() {
+
+
+    // identify tags that can be ignored
+    function canIgnore(tag) {
+        var coreTags = /add|wj|nd/g;        
+        return (tag.match(coreTags) === null);
+    }
+
+    var Verse = function(str) {
+        var re = /(\\\+?(\w+))\*?\s?/gm;
+        var arr = null;
+
+        while ((arr = re.exec(str)) !== null) {
+            if (canIgnore(arr[2])) {
+                continue;
+            }
+
+            console.log(arr);
+        }
+
+        this.text = str;
+        this.tags = [];
+    };
+
+    // var text = '\\zw \\+zws H07225 \\+zws*\\zw*In the beginning\\zx \\zx* \\zw \\+zws H0430 \\+zws*\\zw*God\\zx \\zx* \n' +
+    //     '\\zw \\+zws H0853 H01254 \\+zws*\\+zwm strongMorph:TH8804 \\+zwm*\\zw*created\\zx \\zx* \n' +
+    //     '\\zw \\+zws H08064 \\+zws*\\zw*the heaven\\zx \\zx* \\zw \\+zws H0853 \\+zws*\\zw*and\\zx \\zx* \n' +
+    //     '\\zw \\+zws H0776 \\+zws*\\zw*the earth\\zx \\zx*.';
+
+    var text = '\\zw \\+zws H0776 \\+zws*\\zw*And the earth\\zx \\zx* \\zw \\+zws H01961 \\+zws*\\+zwm strongMorph:TH8804 \\+zwm*\\zw*was\\zx \\zx*' +
+        '\\zw \\+zws H08414 \\+zws*\\zw*without form\\zx \\zx*, \\zw \\+zws H0922 \\+zws*\\zw*and' +
+        'void\\zx \\zx*; \\zw \\+zws H02822 \\+zws*\\zw*and darkness\\zx \\zx* \\add was\\add*' +
+        '\\zw \\+zws H06440 \\+zws*\\zw*upon the face\\zx \\zx* \\zw \\+zws H08415 \\+zws*\\zw*of the' +
+        'deep\\zx \\zx*. \\zw \\+zws H07307 \\+zws*\\zw*And the Spirit\\zx \\zx* \\zw \\+zws H0430 \\+zws*\\zw*of' +
+        'God\\zx \\zx* \\zw \\+zws H07363 \\+zws*\\+zwm strongMorph:TH8764 \\+zwm*\\zw*moved\\zx \\zx*' +
+        '\\zw \\+zws H05921 \\+zws*\\zw*upon\\zx \\zx* \\zw \\+zws H06440 \\+zws*\\zw*the' +
+        'face\\zx \\zx* \\zw \\+zws H04325 \\+zws*\\zw*of the waters\\zx \\zx*.';
+
+    var v = new Verse(text);
+    //console.log(tmp);
+    //console.log(v.tex);
+    //console.log(canIgnore('\\ww'));
+}());
+
+// var moduleBible = require('./lib-modules/bible.js');
+
+// var Verse   = moduleBible.Verse;
+// var Chapter = moduleBible.Chapter;
+
+// //var verse = new moduleBible.Verse();
+
+// var v = new Verse();
+// v.render();
+
+// var c = new Chapter();
+// c.render();
+
+
 
 //var fs   = require('fs');
 // var path = require('path');
@@ -70,6 +115,3 @@ var walk = function(dir, done) {
 //       return !c.toString().match(/[Ա-Ֆ]/gi);
 //    });
 // });
-
-
-}());
