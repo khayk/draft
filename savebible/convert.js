45632d69d01c4e852fd187d0ed9918eb7338a10a
Hayk Karapetyan <hayk.karapetyan@inn.ru>
2014-12-26 21:53:59 +0400

    implemented verse representation

--- savebible/convert.js
+++ savebible/convert.js
@@ -1,85 +1,183 @@
 (function() {
+  /*
+      function skipTag(re, str, skipTo) {
+          var arr = null;
+          while (true) {
+              arr = re.exec(str);
+              if (arr === null) {
+                  throw 'not expected to encounter the end at this point';
+              }
+              if (arr[1] == skipTo)
+                  return;
+          }
+      }
+
+      function findClosingTag(re, arr, str, what) {
+          while (true) {
+              arr = re.exec(str);
+              if (arr === null) {
+                  throw 'not expected to encounter the end at this point';
+              }
+              if (arr[1] == what) {
+                  console.log('pair found: ', arr[1]);
+                  return arr;
+              }
+          }
+
+      }
+  */
+
+  function extend(child, parent) {
+    var fnObj = function() {};
+    fnObj.prototype = parent.prototype;
+    child.prototype = new fnObj();
+    child.prototype.constructor = child;
+    child.uber = parent.prototype;
+  }
+
+  // identify tags that can be ignored
+  function supportedTag(tag) {
+    var coreTags = /add|wj|nd/g;
+    return (tag.match(coreTags) !== null);
+  }
+
+  function openingTag(str) {
+    return str[str.length - 1] !== '*';
+  }
 
 
-    // identify tags that can be ignored
-    function supportedTag(tag) {
-        var coreTags = /add|wj|nd/g;
-        return (tag.match(coreTags) !== null);
+  /// -----------------------------------------------------------------------
+  ///                      NODE - THE BASE CLASS
+  /// -----------------------------------------------------------------------
+  var Node = function(parent) {
+    this.parent = parent;
+    this.nodes = [];
+  };
+
+  Node.prototype.addChild = function(node) {
+    this.nodes.push(node);
+  };
+
+  Node.prototype.toString = function() {
+    var res = '';
+    this.nodes.forEach(function(n) {
+      res += n.toString();
+    });
+    return res;
+  };
+
+  Node.prototype.render = function() {
+
+  };
+
+  /// -----------------------------------------------------------------------
+  ///                             TEXT NODE
+  /// -----------------------------------------------------------------------
+  var TextNode = function(text, parent) {
+    Node.call(this, parent);
+    this.text = text;
+  };
+  extend(TextNode, Node);
+  // TextNode.prototype = Object.create(Node.prototype);
+  // TextNode.prototype.constructor = TextNode;
+
+  TextNode.prototype.toString = function() {
+    return this.text + Node.prototype.toString.call(this);
+  };
+
+  /// -----------------------------------------------------------------------
+  ///                             TAG NODE
+  /// -----------------------------------------------------------------------
+  var TagNode = function(tag, parent) {
+    Node.call(this, parent);
+    this.tag = tag;
+  };
+  extend(TagNode, Node);
+  // TagNode.prototype = Object.create(Node.prototype);
+  // TagNode.prototype.constructor = TagNode;
+
+  TagNode.prototype.toString = function() {
+    return this.tag + ' ' + Node.prototype.toString.call(this) + this.tag + '*';
+  };
+
+
+  function buildTextNode(str, from, to, node) {
+    var text = str.substring(from, to);
+    return new TextNode(text, node);
+  }
+
+  /// -----------------------------------------------------------------------
+  ///                      PARSE VERSE IN USFM FORMAT
+  /// -----------------------------------------------------------------------
+  var USFM_VerseParser = function(str, ind, arr, re, node) {
+    if (re === null) {
+      re = /(\\\+?(\w+)\*?)\s?/gm;
+      arr = re.exec(str);
+      ind = 0;
     }
 
-    function skipTag(re, str, skipTo) {
-        var arr = null;
-        while (true) {
-            arr = re.exec(str);
-            if (arr === null) {
-                throw 'not expected to encounter the end at this point';
-            }
-            if (arr[1] == skipTo)
-                return;
-        }
-    }
+    if (arr !== null) {
 
-    function findClosingTag(re, arr, str, what) {
-        while (true) {
-            arr = re.exec(str);
-            if (arr === null) {
-                throw 'not expected to encounter the end at this point';
-            }
-            if (arr[1] == what) {
-                console.log('pair found: ', arr[1]);
-                return arr;
-            }
-        }
+      // collect the available text
+      if (ind < arr.index) {
+        node.addChild(buildTextNode(str, ind, arr.index, node));
+      }
+
+      var tag = arr[1];
+      if (openingTag(tag)) {
+        var tagNode = new TagNode(tag, node);
+        node.addChild(tagNode);
+        ind = arr.index + arr[0].length;
+        arr = re.exec(str);
+        USFM_VerseParser(str, ind, arr, re, tagNode);
+        return;
+      } else {
+        ind = arr.index + arr[1].length;
+        arr = re.exec(str);
+        USFM_VerseParser(str, ind, arr, re, node.parent);
+        return;
+      }
 
     }
 
-    var Verse = function(str) {
-        var re = /(\\\+?(\w+)\*?)\s?/gm;
-        var arr = null;
-        var start = 0;
+    // collect remaining text
+    if (ind < str.length) {
+      node.addChild(buildTextNode(str, ind, str.length, node));
+    }
+  };
 
-        while ((arr = re.exec(str)) !== null) {
-            if (arr.index > start) {
-                console.log('text found:', str.substring(start, arr.index));
-            }
+  /// -----------------------------------------------------------------------
+  ///                         VERSE CLASS
+  /// -----------------------------------------------------------------------
+  var Verse = function(str, number) {
+    this.number = number;
+    this.node   = new Node(null);
+    USFM_VerseParser(str, 0, null, null, this.node);
+  };
 
-            if (!supportedTag(arr[2])) {
-                /// skip to tag
-                //console.log('tag will be skipped : ', arr[1]);
-                arr = findClosingTag(re, arr, str, arr[1] + '*');
-                start = arr.index + arr[0].length;
-            }
+  // function VerseView() {
+  //   this.display = function(v) {
+  //     var res = v.number + ' ';
+  //     v.node.
+  //   };
+  // }
 
-            start = arr.index + arr[0].length;
+  var orig = '\\zw \\+zws H0776 \\+zws*\\zw*And the earth\\zx \\zx* \\zw \\+zws H01961 \\+zws*\\+zwm strongMorph:TH8804 \\+zwm*\\zw*was\\zx \\zx*' +
+    '\\zw \\+zws H08414 \\+zws*\\zw*without form\\zx \\zx*, \\zw \\+zws H0922 \\+zws*\\zw*and' +
+    'void\\zx \\zx*; \\zw \\+zws H02822 \\+zws*\\zw*and darkness\\zx \\zx* \\add was\\add*' +
+    '\\zw \\+zws H06440 \\+zws*\\zw*upon the face\\zx \\zx* \\zw \\+zws H08415 \\+zws*\\zw*of the' +
+    'deep\\zx \\zx*. \\zw \\+zws H07307 \\+zws*\\zw*And the Spirit\\zx \\zx* \\zw \\+zws H0430 \\+zws*\\zw*of' +
+    'God\\zx \\zx* \\zw \\+zws H07363 \\+zws*\\+zwm strongMorph:TH8764 \\+zwm*\\zw*moved\\zx \\zx*' +
+    '\\zw \\+zws H05921 \\+zws*\\zw*upon\\zx \\zx* \\zw \\+zws H06440 \\+zws*\\zw*the' +
+    'face\\zx \\zx* \\zw \\+zws H04325 \\+zws*\\zw*of the waters\\zx \\zx*.';
 
-            // var sub = str.substring(start);
-            // this.Verse(sub);
-        }
+  //var orig = '\\xy \\add 1\\nd 2\\wj 3\\wj*\\nd*\\add*4\\xy*';
+  //var orig = '\\m 1 \\x 2 \\y 3 \\z 4 \\z* 5 \\y* 6 \\x* 7\\m*';
+  var v = new Verse(orig);
+  var restored = v.node.toString();
 
-        console.log('tailing text: ', str.substring(start, str.length));
-        this.text = str;
-        this.tags = [];
-    };
-
-    // var text = '\\zw \\+zws H07225 \\+zws*\\zw*In the beginning\\zx \\zx* \\zw \\+zws H0430 \\+zws*\\zw*God\\zx \\zx* \n' +
-    //     '\\zw \\+zws H0853 H01254 \\+zws*\\+zwm strongMorph:TH8804 \\+zwm*\\zw*created\\zx \\zx* \n' +
-    //     '\\zw \\+zws H08064 \\+zws*\\zw*the heaven\\zx \\zx* \\zw \\+zws H0853 \\+zws*\\zw*and\\zx \\zx* \n' +
-    //     '\\zw \\+zws H0776 \\+zws*\\zw*the earth\\zx \\zx*.';
-
-    var text = '\\zw \\+zws H0776 \\+zws*\\zw*And the earth\\zx \\zx* \\zw \\+zws H01961 \\+zws*\\+zwm strongMorph:TH8804 \\+zwm*\\zw*was\\zx \\zx*' +
-        '\\zw \\+zws H08414 \\+zws*\\zw*without form\\zx \\zx*, \\zw \\+zws H0922 \\+zws*\\zw*and' +
-        'void\\zx \\zx*; \\zw \\+zws H02822 \\+zws*\\zw*and darkness\\zx \\zx* \\add was\\add*' +
-        '\\zw \\+zws H06440 \\+zws*\\zw*upon the face\\zx \\zx* \\zw \\+zws H08415 \\+zws*\\zw*of the' +
-        'deep\\zx \\zx*. \\zw \\+zws H07307 \\+zws*\\zw*And the Spirit\\zx \\zx* \\zw \\+zws H0430 \\+zws*\\zw*of' +
-        'God\\zx \\zx* \\zw \\+zws H07363 \\+zws*\\+zwm strongMorph:TH8764 \\+zwm*\\zw*moved\\zx \\zx*' +
-        '\\zw \\+zws H05921 \\+zws*\\zw*upon\\zx \\zx* \\zw \\+zws H06440 \\+zws*\\zw*the' +
-        'face\\zx \\zx* \\zw \\+zws H04325 \\+zws*\\zw*of the waters\\zx \\zx*.';
-
-    var test = '\\zw \\zw*\\add hello \\nd to \\wj developer\\wj*\\nd*\\add* some other text';
-    var v = new Verse(test);
-    //console.log(tmp);
-    //console.log(v.tex);
-    //console.log(canIgnore('\\ww'));
+  console.log(orig);
+  console.log(restored);
 }());
 
 // var moduleBible = require('./lib-modules/bible.js');
