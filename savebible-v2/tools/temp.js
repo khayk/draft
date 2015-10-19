3029bae8940f869f5e566f5ea1d9cf7db9dbdd8c
Hayk Karapetyan <hayk.karapetyan@inn.ru>
2015-10-19 17:46:24 +0400

    new search

--- savebible-v2/tools/temp.js
+++ savebible-v2/tools/temp.js
@@ -6,6 +6,7 @@ var cmn  = require('../lib/common');
 var help = require('../helpers');
 var path = require('path');
 var fs   = require('fs-extra');
+var _    = require('lodash');
 
 var measur = new help.Measurer();
 
@@ -18,26 +19,100 @@ var startupInitialization = function() {
 startupInitialization();
 
 
+var Node = function(letter) {
+  this.letter = letter;
+  this.parent = null;
+  this.childs = {};
+};
 
-    var parser = new lb.Parser();
-    var bible  = new lb.Bible();
-    var book   = parser.parseBook('\\id GEN Genesis');
-    var chap   = parser.parseChapter('\\c 1');
-    var chap2  = parser.parseChapter('\\c 2');
+Node.prototype.addNode = function(node) {
+  var r = this.childs[node.letter];
+  if (_.isUndefined(r)) {
+    this.childs[node.letter] = node;
+    node.parent = this;
+  }
+};
 
+Node.prototype.optimize = function() {
+  _.each(this.childs, function(value, key) {
+    value.optimize();
+  });
 
-    book.index = lb.BBM.instance().onById(book.te.id);
-    //book.addChapter(chap);
-    bible.addBook(book);
-    //book.addChapter(chap2);
+  if (!_.isUndefined(this.refs)) {
+    this.refs.sort();
+    this.refs = _.unique(this.refs, true);
+  }
+};
 
-    var vit = bible.verseIterator();
-    var v = null;
-    while ((v = vit.next()) !== null) {
-      console.log(v.id());
-    }
+Node.prototype.getNode = function(letter) {
+  var r = this.childs[letter];
+  if (_.isUndefined(r))
+    return null;
+  return r;
+};
+
+Node.prototype.addRef = function(ref) {
+  if (_.isUndefined(this.refs))
+    this.refs = [];
+  this.refs.push(ref);
+};
+
+var NDictionary = function() {
+  this.root = new Node('');
+};
+
+NDictionary.prototype.optimize = function() {
+  this.root.optimize();
+};
+
+NDictionary.prototype.find = function(word) {
+  var node = this.root;
+  for (var i = 0; i < word.length; i++) {
+    var letter = word[i];
+    var child = node.getNode(letter);
+    if (child === null)
+      return null;
+    node = child;
+  }
+  if (_.isUndefined(node.refs))
+    return null;
+  return node.refs;
+};
+
+NDictionary.prototype.add = function(word, ref) {
+  if (word.length === 0)
     return;
 
+  var node = this.root;
+  for (var i = 0; i < word.length; i++) {
+    var letter = word[i];
+    var child = node.getNode(letter);
+    if (child === null) {
+      child = new Node(letter);
+      node.addNode(child);
+    }
+    node = child;
+  }
+  node.addRef(ref);
+  return this;
+};
+
+
+var dict  = new NDictionary();
+var text = 'test case TEST';
+var wordsArray = text.split(' ');
+for (var i = 0; i < wordsArray.length; ++i)
+  dict.add(wordsArray[i], i.toString());
+
+console.log(dict.find('test'));
+console.log(dict.find('case'));
+console.log(dict.find('TEST'));
+console.log(dict.find('case1'));
+console.log(dict.find('cas'));
+console.log(dict.find('Test'));
+
+//console.log(require('util').inspect(dict.root, {depth: 15, colors: true}));
+return;
 
 var opts = [
   //{folder: 'usfm',   extension: '.usfm', renderer: new rndr.UsfmRenderer()                     },
@@ -64,7 +139,10 @@ if (meta === null)
   throw new Error('Bible language is not specified or supported: ' + bible.lang);
 
 var lexic = meta.lex;
-var dict  = new srch.Dictionary('original words');
+
+var dict  = new NDictionary();
+//var dict  = new srch.Dictionary('original words');
+
 var vit   = bible.verseIterator();
 var verse;
 while ((verse = vit.next()) !== null) {
@@ -82,19 +160,21 @@ measur.end();
 
 
 
-measur.begin('search module');
-var search = new srch.Search();
-var vit    = bible.verseIterator();
-var verse;
-while ((verse = vit.next()) !== null) {
-  var text = verse.render(textRndr);
-  var ref  = lb.encodeRef(verse.ref());
-  text     = lexic.removePunctuations(text);
 
-  // process every single word
-  var wordsArray = text.split(' ');
-  for (var i = 0; i < wordsArray.length; ++i)
-    search.add(wordsArray[i], ref);
-}
-search.buildIndex();
-measur.end();
+
+// measur.begin('search module');
+// var search = new srch.Search();
+// var vit    = bible.verseIterator();
+// var verse;
+// while ((verse = vit.next()) !== null) {
+//   var text = verse.render(textRndr);
+//   var ref  = lb.encodeRef(verse.ref());
+//   text     = lexic.removePunctuations(text);
+
+//   // process every single word
+//   var wordsArray = text.split(' ');
+//   for (var i = 0; i < wordsArray.length; ++i)
+//     search.add(wordsArray[i], ref);
+// }
+// //search.buildIndex();
+// measur.end();
